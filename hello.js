6fe421c441e38aeed75a26ff449098f63ab1a8d4
Sarah-Cashin <62601861+Sarah-Cashin@users.noreply.github.com>
2020-10-22 08:05:22 -0700

    Fourth commit
    Trying to use similar code in tracker app to read JSON response (getData and result) error message in developer tool saying TypeError: cannot convert undefined or null to object

--- hello.js
+++ hello.js
@@ -1,6 +1,3 @@
-// const header = document.querySelector('header');
-// const section = document.querySelector('section');
-
 const myPromise = new Promise((resolve, reject) => {
     let hello = true;
 
@@ -11,71 +8,29 @@ const myPromise = new Promise((resolve, reject) => {
     }
 });
 
-myPromise.then((message) => {
-    console.log(message);
-}).catch((message) => {
-    console.log(message);
-})
+// myPromise.then((message) => {
+//     console.log(message);
+// }).catch((message) => {
+//     console.log(message);
+// })
 
+const api = 'https://content.guardianapis.com/search?api-key=930de680-1e77-42b1-90eb-1077cac50341';
 
-const news = new Promise((resolve, reject) => {
-    fetch("https://content.guardianapis.com/search?api-key=930de680-1e77-42b1-90eb-1077cac50341", {
+const getData = async() => {
+    const response = await fetch(`${api}`);
+    if (response.ok) {
+        return await response.json();
+    } else {
+        return Promise.reject(response.status);
+    }
+};
 
-        })
-        .then(response => {
-            console.log(response);
-
-        }).catch(err => {
-            console.error(err);
-        });
-});
-
-// let requestURL = 'https://content.guardianapis.com/search?q=results&api-key=930de680-1e77-42b1-90eb-1077cac50341';
-// let request = new XMLHttpRequest();
-// request.open('GET', requestURL);
-
-// request.responseType = 'json';
-// request.send();
-
-// request.onload = function() {
-//     const news = request.response;
-//     populateHeader(news);
-//     showNews(news);
-// }
-
-// function populateHeader(jsonObj) {
-//     const myH1 = document.createElement('h1');
-//     myH1.textContent = jsonObj['webTitle'];
-//     header.appendChild(myH1);
-
-//     const myPara = document.createElement('p');
-//     myPara.textContent = 'sectionName' + jsonObj['webTitle'];
-//     header.appendChild(myPara);
-// }
-
-// function showNews(jsonObj) {
-//     const headLine = jsonObj['results'];
-
-//     for (let i = 0; i < headLine.length; i++) {
-//         const myArticle = document.createElement('article');
-//         const myH2 = document.createElement('h2');
-//         const myPara1 = document.createElement('p');
-//         const myPara2 = document.createElement('p');
-//         const myPara3 = document.createElement('p');
-
-//         myH2.textContent = headLine[i].type;
-//         myPara1.textContent = 'sectionName: ' + headLine[i].sectionName;
-//         myPara2.textContent = 'webPublicationDate: ' + headLine[i].webPublicationDate;
-//         myPara3.textContent = 'webTitle:';
-
-//     }
-
-
-//     myArticle.appendChild(myH2);
-//     myArticle.appendChild(myPara1);
-//     myArticle.appendChild(myPara2);
-//     myPromise.appendChild(myPara3);
-
-//     section.appendChild(myArticle);
-
-// }
+const result = getData();
+result
+    .then((data) => {
+        let sectionName = Object.keys(data.sectionName);
+        let webPublicationDate = Object.keys(data.webPublicationDate);
+        let webTitle = object.keys(data.webTitle);
+        let webUrl = object.keys(data.webUrl);
+        console.log(data);
+    })
